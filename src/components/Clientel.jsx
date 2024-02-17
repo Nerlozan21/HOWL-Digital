a20b6c5db5c314ea4754604c559ad6555a24bbbb
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2024-02-17 16:26:10 +0530

    addition of Clientel

---
+++ src/components/Clientel.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/ClientelStyles.css";
+
+const Clientel = () => {
+  return (
+    <div className="clentel-cntr">
+      <h2>Clients We Work With</h2>
+      <div className="main-img-cntr">
+        <div className="img-cntr1">
+          <img src="/src/assets/clientel/hrx1.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/AMEX-Cards-1.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/schnider.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/TOI.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/Baggit.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/TrueBlue.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/AQBottle.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/Floh.jpg" alt="" srcset="" width={135} height={200}/>
+        </div>
+        <div className="img-cntr2">
+          <img src="/src/assets/clientel/Almond-house.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/DBS.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/aadyam.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/IST-Howl-Website-Thumbnail-03.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/TSGC-image-1.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/btwn-US.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/IVAPE-1.jpg" alt="" srcset="" width={135} height={200}/>
+          <img src="/src/assets/clientel/thumb-baby.jpg" alt="" srcset="" width={135} height={200}/>
+       </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clientel;
